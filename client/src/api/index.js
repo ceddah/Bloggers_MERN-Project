20bbaeab86e0667daa022e31e2a1f7996583b779
ceddah <cedanik1@gmail.com>
2022-09-14 18:27:53 +0200

    url fix

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import { SERVER_URL } from "../constants/routes";
 
 export const signUp = async (formData) => {
-  return await fetch(`${SERVER_URL}/api/auth/sign-up`, {
+  return await fetch(`${SERVER_URL}/auth/sign-up`, {
     method: "POST",
     credentials: "include",
     headers: {
@@ -12,7 +12,7 @@ export const signUp = async (formData) => {
 };
 
 export const signIn = async (formData) => {
-  return await fetch(`${SERVER_URL}/api/auth/sign-in`, {
+  return await fetch(`${SERVER_URL}/auth/sign-in`, {
     method: "POST",
     credentials: "include",
     headers: {
@@ -26,21 +26,21 @@ export const fetchCurrentUser = async () => {
   const user = JSON.parse(localStorage.getItem("user"));
   if (!user) return null;
 
-  return await fetch(`${SERVER_URL}/api/auth/me`, {
+  return await fetch(`${SERVER_URL}/auth/me`, {
     method: "GET",
     credentials: "include",
   });
 };
 
 export const signOut = async () => {
-  return await fetch(`${SERVER_URL}/api/auth/sign-out`, {
+  return await fetch(`${SERVER_URL}/auth/sign-out`, {
     method: "GET",
     credentials: "include",
   });
 };
 
 export const getLatestPosts = async () => {
-  return await fetch(`${SERVER_URL}/api/posts/latest-posts`, {
+  return await fetch(`${SERVER_URL}/posts/latest-posts`, {
     method: "GET",
     credentials: "include",
   });
@@ -48,7 +48,7 @@ export const getLatestPosts = async () => {
 
 export const getAllPosts = async (search, category, page) => {
   return await fetch(
-    `${SERVER_URL}/api/posts/browse-posts?page=${page}&category=${category}&search=${search}`,
+    `${SERVER_URL}/posts/browse-posts?page=${page}&category=${category}&search=${search}`,
     {
       method: "GET",
       credentials: "include",
@@ -57,7 +57,7 @@ export const getAllPosts = async (search, category, page) => {
 };
 
 export const postNewPost = async (postData) => {
-  return await fetch(`${SERVER_URL}/api/posts/create-new`, {
+  return await fetch(`${SERVER_URL}/posts/create-new`, {
     method: "POST",
     credentials: "include",
     headers: {
@@ -68,7 +68,7 @@ export const postNewPost = async (postData) => {
 };
 
 export const postDetail = async (postId) => {
-  return await fetch(`${SERVER_URL}/api/posts/detail/${postId}`, {
+  return await fetch(`${SERVER_URL}/posts/detail/${postId}`, {
     method: "GET",
     credentials: "include",
   });
@@ -77,7 +77,7 @@ export const postDetail = async (postId) => {
 // comments
 
 export const postNewComment = async (postId, newComment) => {
-  return await fetch(`${SERVER_URL}/api/posts/detail/${postId}/new-comment`, {
+  return await fetch(`${SERVER_URL}/posts/detail/${postId}/new-comment`, {
     method: "POST",
     credentials: "include",
     headers: {
@@ -88,7 +88,7 @@ export const postNewComment = async (postId, newComment) => {
 };
 
 export const postEditComment = async (commentId, newComment) => {
-  return await fetch(`${SERVER_URL}/api/posts/detail/edit-comment/${commentId}`, {
+  return await fetch(`${SERVER_URL}/posts/detail/edit-comment/${commentId}`, {
     method: "POST",
     credentials: "include",
     headers: {
@@ -99,28 +99,28 @@ export const postEditComment = async (commentId, newComment) => {
 };
 
 export const deleteComment = async (postId, commentId) => {
-  return await fetch(`${SERVER_URL}/api/posts/detail/${postId}/remove-comment/${commentId}`, {
+  return await fetch(`${SERVER_URL}/posts/detail/${postId}/remove-comment/${commentId}`, {
     method: "DELETE",
     credentials: "include",
   });
 };
 
 export const getLikeComment = async (commentId) => {
-  return await fetch(`${SERVER_URL}/api/posts/detail/like-comment/${commentId}`, {
+  return await fetch(`${SERVER_URL}/posts/detail/like-comment/${commentId}`, {
     method: "GET",
     credentials: "include",
   });
 };
 
 export const getBookmarkPost = async (postId) => {
-  return await fetch(`${SERVER_URL}/api/posts/bookmark/${postId}`, {
+  return await fetch(`${SERVER_URL}/posts/bookmark/${postId}`, {
     method: "GET",
     credentials: "include",
   });
 };
 
 export const getAllBookmarks = async (currentPage) => {
-  return await fetch(`${SERVER_URL}/api/posts/my-bookmarks?page=${currentPage}`, {
+  return await fetch(`${SERVER_URL}/posts/my-bookmarks?page=${currentPage}`, {
     method: "GET",
     credentials: "include",
   });
@@ -129,7 +129,7 @@ export const getAllBookmarks = async (currentPage) => {
 //user
 
 export const getUserProfileDetails = async (userId) => {
-  return await fetch(`${SERVER_URL}/api/user-detail/${userId}`, {
+  return await fetch(`${SERVER_URL}/user-detail/${userId}`, {
     method: "GET",
     credentials: "include",
   });
@@ -137,7 +137,7 @@ export const getUserProfileDetails = async (userId) => {
 
 // { currentPassword, newPassword }
 export const postResetPassword = async (data) => {
-  return await fetch(`${SERVER_URL}/api/user-detail/reset-password`, {
+  return await fetch(`${SERVER_URL}/user-detail/reset-password`, {
     method: "POST",
     credentials: "include",
     headers: {
@@ -149,7 +149,7 @@ export const postResetPassword = async (data) => {
 
 // { socials: { facebook: "asdas", twitter: "adas", linkedin: "asdasas" } }
 export const postSetSocial = async (userId, data) => {
-  return await fetch(`${SERVER_URL}/api/user-detail/set-social/${userId}`, {
+  return await fetch(`${SERVER_URL}/user-detail/set-social/${userId}`, {
     method: "POST",
     credentials: "include",
     headers: {
@@ -160,7 +160,7 @@ export const postSetSocial = async (userId, data) => {
 };
 
 export const postUpdateBio = async (newBio) => {
-  return await fetch(`${SERVER_URL}/api/user-detail/set-short-bio`, {
+  return await fetch(`${SERVER_URL}/user-detail/set-short-bio`, {
     method: "POST",
     credentials: "include",
     headers: {
@@ -173,14 +173,14 @@ export const postUpdateBio = async (newBio) => {
 };
 
 export const getSubmitReting = async (postId, rating) => {
-  return await fetch(`${SERVER_URL}/api/posts/detail/${postId}/update-ratings/?rating=${rating}`, {
+  return await fetch(`${SERVER_URL}/posts/detail/${postId}/update-ratings/?rating=${rating}`, {
     method: "GET",
     credentials: "include",
   });
 };
 
 export const postReportBlog = async (postId, reportType) => {
-  return await fetch(`${SERVER_URL}/api/posts/detail/${postId}/report-post`, {
+  return await fetch(`${SERVER_URL}/posts/detail/${postId}/report-post`, {
     method: "POST",
     credentials: "include",
     headers: {
@@ -195,76 +195,70 @@ export const postReportBlog = async (postId, reportType) => {
 // admin
 
 export const getAllUsers = async (currentPage, search) => {
-  return await fetch(
-    `${SERVER_URL}/api/manage/get-all-users?page=${currentPage}&search=${search}`,
-    {
-      method: "GET",
-      credentials: "include",
-    }
-  );
+  return await fetch(`${SERVER_URL}/manage/get-all-users?page=${currentPage}&search=${search}`, {
+    method: "GET",
+    credentials: "include",
+  });
 };
 
 export const getAllBlogs = async (currentPage, search) => {
-  return await fetch(
-    `${SERVER_URL}/api/manage/get-all-blogs?page=${currentPage}&search=${search}`,
-    {
-      method: "GET",
-      credentials: "include",
-    }
-  );
+  return await fetch(`${SERVER_URL}/manage/get-all-blogs?page=${currentPage}&search=${search}`, {
+    method: "GET",
+    credentials: "include",
+  });
 };
 
 export const getAllReports = async () => {
-  return await fetch(`${SERVER_URL}/api/manage/get-all-reports`, {
+  return await fetch(`${SERVER_URL}/manage/get-all-reports`, {
     method: "GET",
     credentials: "include",
   });
 };
 
 export const getBanUnbanUser = async (userId) => {
-  return await fetch(`${SERVER_URL}/api/manage/ban-unban?userId=${userId}`, {
+  return await fetch(`${SERVER_URL}/manage/ban-unban?userId=${userId}`, {
     method: "GET",
     credentials: "include",
   });
 };
 
 export const getPromoteUser = async (userId) => {
-  return await fetch(`${SERVER_URL}/api/manage/promote-user?userId=${userId}`, {
+  return await fetch(`${SERVER_URL}/manage/promote-user?userId=${userId}`, {
     method: "GET",
     credentials: "include",
   });
 };
 
 export const deleteRemovePost = async (postId) => {
-  return await fetch(`${SERVER_URL}/api/manage/remove-post?postId=${postId}`, {
+  return await fetch(`${SERVER_URL}/manage/remove-post?postId=${postId}`, {
     method: "DELETE",
     credentials: "include",
   });
 };
 
 export const getSetTrending = async (postId) => {
-  return await fetch(`${SERVER_URL}/api/manage/set-trending?postId=${postId}`, {
+  return await fetch(`${SERVER_URL}/manage/set-trending?postId=${postId}`, {
     method: "GET",
     credentials: "include",
   });
 };
 
 export const getReportDetail = async (reportId) => {
-  return await fetch(`${SERVER_URL}/api/manage/get-report-detail?reportId=${reportId}`, {
+  return await fetch(`${SERVER_URL}/manage/get-report-detail?reportId=${reportId}`, {
     method: "GET",
     credentials: "include",
   });
 };
 
 export const getCloseReport = async (reportId) => {
-  return await fetch(`${SERVER_URL}/api/manage/close-report?reportId=${reportId}`, {
+  return await fetch(`${SERVER_URL}/manage/close-report?reportId=${reportId}`, {
     method: "GET",
     credentials: "include",
   });
 };
 
 export const deleteRemovePostAndReport = async (postId, reportId) => {
-  return await fetch(`${SERVER_URL}/api/manage/remove-post?postId=${postId}&reportId=${reportId}`, {
+  return await fetch(`${SERVER_URL}/manage/remove-post?postId=${postId}&reportId=${reportId}`, {
     method: "DELETE",
     credentials: "include",
   });
--- client/src/constants/routes.js
+++ client/src/constants/routes.js
@@ -5,7 +5,7 @@ export const BROWSE = "/browse-blogs";
 export const PROFILE = "/profile/:userId";
 export const DETAIL = "/blog-read/:blogId";
 export const ADMIN = "/admin-panel";
-export const SERVER_URL = "http://localhost:3434";
+export const SERVER_URL = "/api";
 
 //admin
 export const ADMIN_WELCOME = "";
